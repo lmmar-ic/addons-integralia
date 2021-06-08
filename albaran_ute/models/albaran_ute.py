5ae0c0733888b91a4b17a45e92d70fe36a44f4e1
pedro@serincloud.com <pedro@serincloud.com>
2021-06-08 12:26:26 +0200

    UTE vol2

--- albaran_ute/models/albaran_ute.py
+++ albaran_ute/models/albaran_ute.py
@@ -7,17 +7,16 @@ from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationE
 class AlbaranUte(models.Model):
     _inherit = 'repair.order'
 
-    fecha_albaran = fields.Date(string='Fecha')
+    fecha_albaran = fields.Date(string='Fecha albarán')
     secuencia_albaran = fields.Char(string='Secuencia')
     orden_trabajo = fields.Char(string='Orden de trabajo')
-    nombre_reparacion = fields.Text(string='Nombre')
+    nombre_reparacion = fields.Text(string='Descripción')
     fecha_notificacion = fields.Date(string='Fecha notificación')
-
 
     def get_hours(self):
         for record in self:
             total = 0
             for li in record.fees_lines:
                 total += li.product_uom_qty
-            record.horas
+            record.horas = total
     horas = fields.Float(string='Horas', compute='get_hours', store=False)
