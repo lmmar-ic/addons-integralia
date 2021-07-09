e871da65a5b38172a47d26cf4b75d19935b170be
lmmar-ic <skf$gRGH%%746>
2021-07-09 08:36:08 +0200

    V0

---
+++ presupuesto_sinpvp/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Presupuesto sin PVP',
+    'version': '14.0.1.0',
+    'category': 'Ventas',
+    'description': u"""
+
+""",
+    'author': 'Serincloud',
+    'depends': ['sale', 'report_qweb_pdf_watermark' ],
+    'data': [
+        'views/templates.xml',
+        'views/sale_order_sinpvp_report.xml',
+    ],
+    'installable': True,
+}
